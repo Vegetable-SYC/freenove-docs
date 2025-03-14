a5148866d92fe23653af6239dcb9c19d047424a0
Suhayl <895761900@qq.com>
2025-03-14 14:29:10 +0800

    Update custom.js

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,73 +1,32 @@
 // JavaScript Document
 $(document).ready(function () {
-	loadReady();
+    loadReady();
 
 });
 $(window).resize(function () {
-	// console.log('resize ... ');
-	NaviResize();
+    NaviResize();
 });
 
 function loadReady() {
-	// console.log('ready ... ');
-	setNavBar();
-	NaviResize();
-	var autoPlay2 = setInterval("test()", 1000);
-	$(".dropBtn").click(function () {
-		// $(this).css("background-color", "#FFF");
-		$("#txt").text("click:");
-	});
+    setNavBar();
+    NaviResize();
 
 }
 
 function NaviResize() {
-	// var wy_nav_side = document.getElementsByClassName('wy-nav-side')[0];
-	// var wy_nav_content = document.getElementsByClassName('wy-nav-content')[0];
-	// // var nav_fn = document.getElementsByClassName('nav_fn')[0];
-	// var rect_wy_nav_side = wy_nav_side.getBoundingClientRect();
-	// // var rect_wy_nav_content = wy_nav_content.getBoundingClientRect();
-	// // var rect_nav_fn = nav_fn.getBoundingClientRect();
-	// console.log('rect_wy_nav_side :', rect_wy_nav_side.width);
-	// console.log('wy_nav_content:', wy_nav_content.width);
-	// $('.wy-nav-content').on
-	// // var winWidth = $(window).width();
-	// console.log("wy-nav-side " + $('.wy-nav-side').width() + ' wy-nav-content: ' + $('.wy-nav-content').outerWidth(true) + " " + $('.wy-nav-side').offset().left + "");
-	var navWidth = $('.wy-nav-side').width() + $('.wy-nav-content').outerWidth(true) + $('.wy-nav-side').offset().left;
-	var navHeight = $(".extrabody-content").height();
-	var navItemWidth = navWidth / 7;
-	navItemWidth = navItemWidth < 60 ? 60 : navItemWidth;
-	var navItemHeight = navItemWidth / 5;
-	// console.log("navWidth " + navWidth + ' navItemWidth: ' + navItemWidth + " " + $('.wy-nav-side').offset().left + "");
-	// $(".extrabody-content").height(navItemWidth);
-	// $(".nav_fn").height(navItemHeight);
-	// $(".nav_fn>ul").css("line-height", navItemHeight + "px");
-	$(".nav_fn>ul>li").width(navItemWidth);
-	// console.log('.extrabody-content.height: ' + $(".extrabody-content").height());
-	$(".nav_fn>ul>li").height($(".extrabody-content").height);
-	$(".nav_fn>ul a").css("font-size", navItemWidth * 0.15 + "px");
+    var navWidth = $('.wy-nav-side').width() + $('.wy-nav-content').outerWidth(true) + $('.wy-nav-side').offset().left;
+    var navHeight = $(".extrabody-content").height();
+    var navItemWidth = navWidth / 7;
+    navItemWidth = navItemWidth < 60 ? 60 : navItemWidth;
+    var navItemHeight = navItemWidth / 5;
+    $(".nav_fn>ul>li").width(navItemWidth);
+    $(".nav_fn>ul>li").height($(".extrabody-content").height);
+    $(".nav_fn>ul a").css("font-size", navItemWidth * 0.15 + "px");
 
-}
-function test() {
-	//var today=new Date()
-	//var h=today.getHours()
-	//var m=today.getMinutes()
-	//var s=today.getSeconds()
-	$("#txt").fadeToggle(1000);
-	//$("#txt").text(bannerCount+":"+winWidth);
-}
-
-function setPageLogo() {
-	var link =
-		document.querySelector("link[rel*='icon']") ||
-		document.createElement("link");
-	link.type = "image/x-icon";
-	link.rel = "shortcut icon";
-	link.href = "_static/images/logo_head_16.png";
-	document.getElementsByTagName("head")[0].appendChild(link);
 }
 
 let navBarHtml =
-	`
+    `
   <div class="nav_fn">
     <ul>
         <!-- <li class="navLogo"><a href="/index.html"></a></li> -->
@@ -150,21 +109,15 @@ let footerHtml = `
 </div>`;
 
 function setNavBar() {
-	let navBar = document.getElementById('navContent');
-
-	console.log("111: " + navBar.innerHTML);
-	navBar.innerHTML = navBarHtml;
-	console.log("222: " + navBar.innerHTML);
+    let navBar = document.getElementById('navContent');
+    navBar.innerHTML = navBarHtml;
 }
 
 window.onload = function () {
-
-	// console.log('window.onload');
-	// setPageLogo();
-
+    
     console.log('window.onload');
     let footer_content = document.getElementById('footer_content');
-	footer_content.innerHTML = footerHtml;
+    footer_content.innerHTML = footerHtml;
     $("#copy_right").text("© CopyRight 2016 - " + new Date().getFullYear() + ", Freenove")
 };
 
