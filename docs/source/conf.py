046d4dc38d300451c1a7be63c3161d16add247c3
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-03-17 16:56:14 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,6 +99,7 @@ html_css_files = [
     'https://cdn.jsdelivr.net/gh/Freenove/freenove-docs/docs/source/_static/css/custom.css',
     'https://cdn.jsdelivr.net/gh/Freenove/freenove-docs/docs/source/_static/css/navigationStyle.css',
 ]
+
 html_js_files = [
     'https://cdn.jsdelivr.net/gh/Freenove/freenove-docs/docs/source/_static/js/custom.js',
     # 'js/custom.js'
