6275a5c1b52d923c618b88c02e45f6de236a5a6c
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-03-18 08:48:16 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@ sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 project = 'freenove-docs'
 copyright = '2016-2025, Freenove'
 author = 'Freenove'
+release = 'v1.0.0'
 version = 'v1.0.0'
 
 # -- General configuration ---------------------------------------------------
